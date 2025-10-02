ea67e49446d720a55c734d536aa51f9afaa12053
Hishamew <1176663569@qq.com>
2025-10-02 03:25:25 +0000

    add simple power consummer

--- satsim/simulation/power/__init__.py
+++ satsim/simulation/power/__init__.py
@@ -1,2 +1,3 @@
+from .power_sink import *
 from .simple_battery import *
 from .simple_solar_panel import *
---
+++ satsim/simulation/power/power_sink.py
@@ -0,0 +1,43 @@
+__all__ = ['PowerSink']
+
+from typing import TypedDict
+
+import torch
+
+from satsim.architecture import Module
+
+from ..base.battery_base import BatteryStateDict, PowerNodeMixin
+
+
+class SimplePowerSinkStateDict(TypedDict):
+    pass
+
+
+class SimplePowerSink(Module[SimplePowerSinkStateDict], PowerNodeMixin):
+
+    def __init__(self, *args, power_efficiency: torch.Tensor,
+                 **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.register_buffer(
+            '_power_efficiency',
+            power_efficiency,
+            persistent=False,
+        )
+
+    @property
+    def power_efficiency(self) -> torch.Tensor:
+        return self.get_buffer('_power_efficiency')
+
+    def forward(
+        self,
+        state_dict: SimplePowerSinkStateDict,
+        *args,
+        battery_state_dict: BatteryStateDict,
+        **kwargs,
+    ) -> None:
+
+        power_status, battery = self.update_power_status(
+            self.power_efficiency, battery_state_dict)
+
+        return state_dict, (power_status, battery)
